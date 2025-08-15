7c91bb3e2be939a431a7fa3685deff0af586d03e
RajHiremath-510 <raj.hiremath2004@gmail.com>
2025-08-15 19:03:18 +0000

    Initial commit: decoder3to8 OpenLane full project

--- config.tcl
+++ config.tcl
@@ -1,31 +1,17 @@
-# ========== Basic Info ==========
 set ::env(DESIGN_NAME) decoder3to8
 set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/src/decoder3to8.v"
 
-# ========== PDK & Library ==========
-set ::env(PDK) "sky130A"
-set ::env(STD_CELL_LIBRARY) "sky130_fd_sc_hd"
-
-# ========== Clock ==========
-# Even though it's combinational, OpenLane requires a clock
-set ::env(CLOCK_PORT) "clk"
-set ::env(CLOCK_PERIOD) "10"
-
-# ========== Floorplan ==========
+# No clock for pure combinational design
+set ::env(CLOCK_PORT) ""
+set ::env(CLOCK_NET)   ""
+# Floorplan: make area big enough for PDN
+set ::env(FP_SIZING) "absolute"   ;# fixed die size
+set ::env(DIE_AREA) "0 0 200 200" ;# enough space for PDN pitch
+# Floorplan settings
 set ::env(FP_CORE_UTIL) 30
-set ::env(FP_ASPECT_RATIO) 1
-set ::env(FP_CORE_MARGIN) 5
-set ::env(FP_IO_MODE) 1
-set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) "0 0 120 120"
+set ::env(FP_ASPECT_RATIO) 1.0
+set ::env(FP_CORE_MARGIN) 2
 
-# ========== Placement & Routing ==========
-set ::env(PL_TARGET_DENSITY) 0.35
-
-
-# ========== PDN ==========
-set ::env(FP_PDN_ENABLE) 0
+# Optional: skip CTS since no clock
+set ::env(RUN_CTS) 0
 set ::env(RUN_POWER_GRID) 0
-set ::env(RUN_PDN) 0
-# ========== Signoff ==========
-set ::env(RUN_MAGIC_DRC) 1
