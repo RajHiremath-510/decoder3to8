23a6d38813144cdd5b47ffd7220458f5fee3269c
RajHiremath-510 <raj.hiremath2004@gmail.com>
2025-08-15 19:15:43 +0000

    Initial commit: decoder3to8 OpenLane full project

---
+++ runs/run1/config_in.tcl
@@ -0,0 +1,17 @@
+set ::env(DESIGN_NAME) decoder3to8
+set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/src/decoder3to8.v"
+
+# No clock for pure combinational design
+set ::env(CLOCK_PORT) ""
+set ::env(CLOCK_NET)   ""
+# Floorplan: make area big enough for PDN
+set ::env(FP_SIZING) "absolute"   ;# fixed die size
+set ::env(DIE_AREA) "0 0 200 200" ;# enough space for PDN pitch
+# Floorplan settings
+set ::env(FP_CORE_UTIL) 30
+set ::env(FP_ASPECT_RATIO) 1.0
+set ::env(FP_CORE_MARGIN) 2
+
+# Optional: skip CTS since no clock
+set ::env(RUN_CTS) 0
+set ::env(RUN_POWER_GRID) 0
